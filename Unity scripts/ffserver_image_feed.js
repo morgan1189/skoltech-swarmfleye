3785ead9d278aaf5a459f980b8cc8ab8e1871021
simama <simci10@gmail.com>
2015-08-03 12:11:07 +0300

    Video feed from ffserver
    Script to get the image sequence from ffserver in the local network.

---
+++ Unity scripts/ffserver_image_feed.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+	// Continuously get the latest webcam shot from the url provided (ffserver on the local network)
+	// and DXT compress them at runtime
+	var url = "http://192.168.1.34:8090/test.jpg";
+
+	function Start () {
+		// Create a texture in DXT1 format
+		GetComponent.<Renderer>().material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+		while(true) {
+			// Start a download of the given URL
+			var www = new WWW(url);
+
+			// wait until the download is done
+			yield www;
+
+			// assign the downloaded image to the main texture of the object
+			www.LoadImageIntoTexture(GetComponent.<Renderer>().material.mainTexture);
+		}
+	}
+	
